21d6546878f2fe1449f128a0b7219e50eb66f327
yhteoh <yhteoh@uwaterloo.ca>
2019-10-24 12:12:01 -0400

    Added how to change unique_bases in the test.py file

--- test.py
+++ test.py
@@ -13,8 +13,10 @@ i = np.complex(0, 1)
 
 rho = np.kron(np.array([[1, -i], [i, 1]]) / 2, np.array([[1, 0], [0, 0]]))
 
+unique_bases = np.array([["X","X"],["Z","X"],["X","Z"],["Y","Y"],["Z","Z"]])
+
 data, rho, bases, unique_bases = qst.data.create_data(
-    1000, N, rho, unitary_dict=qst.data.dflt_unitary_dict, rotation_error=[0.01, 0.01]
+    1000, N, rho, unique_bases, unitary_dict=qst.data.dflt_unitary_dict, rotation_error=[0.01, 0.01]
 )
 
 data = qst.data.measurement_error(data, 0.01, 0.02)
